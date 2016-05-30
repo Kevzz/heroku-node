cd6a4bf6c207c1713ae8e545515322573d6e36cb
Kevin Dominguez <kuvin93@gmail.com>
2016-05-30 17:55:21 -0500

    creo que quite lo de scrollable por que creo que no se usa

--- app/scripts/core/theme.js
+++ app/scripts/core/theme.js
@@ -7,7 +7,6 @@ angular
     'ngAnimate',
     'easypiechart',
     'NgSwitchery',
-    'sun.scrollable',
     'ui.bootstrap',
     'ui.select',
     'theme.core.templates',
@@ -18,12 +17,7 @@ angular
     'theme.core.messages_controller',
     'theme.core.notifications_controller',
   ])
-  .constant('nanoScrollerDefaults', {
-    // nanoClass: 'scroll-pane',
-    // paneClass: 'scroll-track',
-    // sliderClass: 'scroll-thumb',
-    // contentClass: 'scroll-content'
-  })
+ 
   .run(['$window', function ($window) {
     $window.ngGrid.config = {
         footerRowHeight: 40,
