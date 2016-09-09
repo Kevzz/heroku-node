67bb22f7fd0babb2bf3a8adb22beb182d1c39d92
Kevin Dominguez <kuvin93@gmail.com>
2016-09-09 12:13:34 -0500

    change some controllers

--- app/scripts/core/theme.js
+++ app/scripts/core/theme.js
@@ -10,10 +10,12 @@ angular.module('theme', [
     'ui.select',
     'ng-token-auth',
     'ui.router',
+    'theme.core.templates',
+    'theme.core.template_overrides',
     'theme.core.directives',
     'theme.core.main_controller',
-    'theme.core.products_controller',
     'theme.core.navigation_controller',
+    'theme.core.products_controller',
     'theme.core.messages_controller',
     'theme.core.notifications_controller'
   ])
